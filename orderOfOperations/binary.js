2dad247928ecc1bd022b77b50b2a11f556eb83e1
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-03-24 17:52:13 -0400

    reversed order of binary

--- orderOfOperations/binary.js
+++ orderOfOperations/binary.js
@@ -1,9 +1,11 @@
 const { lexer } = require('../lexer/lexer');
 
+
+//test!
 class Node {
-  constructor(type, value, tokens) {
+  constructor(type, tokens) {
     this.type = type,
-    this.value = value,
+    //this.value = value,
     this.tokens = tokens,
     this.operation = null,
     this.left = null,
@@ -13,7 +15,7 @@ class Node {
 
 let str = 'abc&def&hij&klm&v&j';
 let tokenArray = lexer(str);
-let head = new Node('BINARY', str, tokenArray, null);
+let head = new Node('BINARY', tokenArray, null);
 
 
 function binary(node) {
@@ -24,33 +26,28 @@ function binary(node) {
     'BICONDITIONAL',
   ];
   let tokens = node.tokens;
-  let i = 0;
+  let i = tokens.length -1;
   let found = false;
-  let str = node.value;
-  while(found === false && i < tokens.length) {
+  while(found === false && i >= 0) {
     if (binaryNames.includes(tokens[i].name)) {
-      let position = tokens[i].position;
-      if (i > 0) {
-        position -= tokens[i-1].position;
-      }
-      let leftStr = str.slice(0, position);
+      //let leftStr = tokens[i+1].value;
       let leftArr = tokens.slice(0, i);
-      let left = new Node('BINARY', leftStr, leftArr, null);
+      let left = new Node('BINARY', leftArr, null);
       node.left = left;
-      let rightStr = str.slice(position + 1);
-      let rightArr = tokens.slice(i +1);
-      let right = new Node('BINARY', rightStr, rightArr, null);
+      //let rightStr = str.slice(position + 1);
+      let rightArr = tokens.slice(i + 1);
+      let right = new Node('BINARY', rightArr, null);
       node.right = right;
       node.operation = tokens[i].name;
       found = true;
     } else {
-      i ++;
+      i --;
     }
   }
   if (found) {
-    binary(node.right);
+    binary(node.left);
   }
 }
 
 binary(head);
-console.log(head.right.right.right);
+console.log(head.left.left.left.left);
