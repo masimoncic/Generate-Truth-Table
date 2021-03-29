713ec37489fb3c2fde4a798432b5918714f3acc1
Mark Simoncic <63131950+masimoncic@users.noreply.github.com>
2021-03-29 10:15:44 -0400

    changed name

--- index.js
+++ index.js
@@ -87,4 +87,25 @@ function generateTruthTable (str) {
   console.log(table);
 }
 
-generateTruthTable('a&(b==c)')
+module.exports = {
+  
+}
+
+//generateTruthTable('a&(b==c)')
+
+
+/*
+function makeTable(array) {
+  var table = document.createElement('table');
+  for (var i = 0; i < array.length; i++) {
+      var row = document.createElement('tr');
+      for (var j = 0; j < array[i].length; j++) {
+          var cell = document.createElement('td');
+          cell.textContent = array[i][j];
+          row.appendChild(cell);
+      }
+      table.appendChild(row);
+  }
+  return table;
+}
+*/
